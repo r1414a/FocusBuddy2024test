273f713edcd023ff27629332d86471ae8cf03153
r1414a <rupeshchincholkar14@gmail.com>
2024-09-30 19:09:17 +0530

    last name error

--- focusbuddybackend/passportJs/passportGoogleStrategy.js
+++ focusbuddybackend/passportJs/passportGoogleStrategy.js
@@ -68,7 +68,7 @@ passport.use(
             availabilityStatus: "No one",
             quiteModeMatchAllowed: true,
             givenName: profile.name.givenName,
-            familyName: profile.name.familyName === '' ? Math.floor(Math.random() * 101) : profile.name.familyName,
+            familyName: profile.name.familyName === undefined ? Math.floor(Math.random() * 101) : profile.name.familyName,
             profilePic: profile.photos[0].value,
             userLocation: location,
             memberSince: new Date(),
